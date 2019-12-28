da413c8802d1a148a499b4560d1a251771b8239a
andreybaglai <andreybaglai@gmail.com>
2019-12-28 12:52:58 +0200

    Get location data from https://ipinfo.io/

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123);
+function init() {
+    const LOCATION_API_TOKEN = '2438c6f33c5dbd';
+
+    fetch(`https://ipinfo.io/json?token=${LOCATION_API_TOKEN}`)
+        .then(res => res.json())
+        .then(location => {
+            console.log(location);
+        });
+}
+
+init();
