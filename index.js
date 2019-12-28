bcaeca90eb6c7d6e0ca20b19eff374a0efff9c67
andreybaglai <andreybaglai@gmail.com>
2019-12-28 21:26:04 +0200

    Get weather from https://api.darksky.net/forecast/

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function init() {
+function getUserLocation() {
     const LOCATION_API_TOKEN = '2438c6f33c5dbd';
 
-    fetch(`https://ipinfo.io/json?token=${LOCATION_API_TOKEN}`)
-        .then(res => res.json())
+    return fetch(`https://ipinfo.io/json?token=${LOCATION_API_TOKEN}`)
+        .then(res => res.json());
+}
+
+function getWeatherForecast(locationCoordinates) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const WEATHER_API_TOKEN = '7ea19e8e10159b02611bc4f0b01a90fc/';
+
+    return fetch(`${proxy}https://api.darksky.net/forecast/${WEATHER_API_TOKEN}${locationCoordinates}`)
+        .then(res => res.json());
+}
+
+function init() {
+    getUserLocation()
         .then(location => {
-            console.log(location);
+            console.log('location: ', location);
+            const { loc } = location;
+            getWeatherForecast(loc)
+                .then(forecast => {
+                    console.log('forecast: ', forecast);
+                });
         });
 }
 
