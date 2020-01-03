5b343d403c7eb2da234a987337774f23f4bef4cb
andreybaglai <andreybaglai@gmail.com>
2020-01-03 15:16:45 +0200

    Add files in folder assets

---
+++ assets/js/UI/UI.js
@@ -0,0 +1,48 @@
+const renderUI = {
+    renderControls = () => {
+
+    },
+
+    renderRefreshBtn = () => {
+
+    },
+
+    renderSelectLanguages = () => {
+
+    },
+
+    renderTemperatureBtns = () => {
+
+    },
+
+    renderSearchBlock = () => {
+
+    },
+
+    /*Main Content*/
+    renderMainContent = () => {
+
+    },
+
+    renderState = () => {
+
+    },
+
+    renderCurrentDate = () => {
+
+    },
+
+    renderMainForecast = () => {
+
+    },
+
+    renderMap = () => {
+
+    },
+
+    renderCoordinates = () => {
+
+    }
+}
+
+export default renderUI;
